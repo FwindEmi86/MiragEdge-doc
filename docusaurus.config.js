558a082adf6433d849cecaa9c059d13fd3269576
FwindEmi <2011857087@qq.com>
2025-04-12 02:23:19 +0800

    fix bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,11 +58,6 @@ const config = {
           alt: 'MiragEdge', // 网站 Logo 的替代文本
           src: 'B-LOGO.svg',
         },
-        algolia: {
-            apiKey: "Search-Only API Key",
-            appId: "Application ID",
-            indexName: "索引名称",
-        },
         items: [
           {
             type: 'docSidebar', // 使用文档侧边栏
