8657df003de5ea1dc674547543a7539d37818541
HFwindEmi <2011857087@qq.com>
2025-04-19 00:44:38 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,6 +111,7 @@ const config = {
             ],
           },
         ],
+		    <a href="https://beian.miit.gov.cn" id="beian" target="_blank">苏ICP备2024133820号-1</a>
         copyright: `版权所有 © ${new Date().getFullYear()} 锐界幻境. 使用 Docusaurus 构建。`,
       },
       prism: {
