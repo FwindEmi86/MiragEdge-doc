3806ea7002d78bed7268056926185d2a804a19db
HFwindEmi <2011857087@qq.com>
2025-04-19 19:06:12 +0800

    g

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark', // 默认深色
-        disableSwitch: true, // 显示切换按钮
+        disableSwitch: false, // 显示切换按钮
         respectPrefersColorScheme: false, // 忽略系统主题设置
       },
       footer: {
